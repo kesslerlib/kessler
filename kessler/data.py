9531e542281790868f793b892c594c22c9aede49
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2020-08-06 09:06:04 +0100

    save traces

--- kessler/data.py
+++ kessler/data.py
@@ -1,5 +1,6 @@
 import uuid
 import os
+import torch
 
 from . import util
 from .models import ConstellationPair
@@ -8,15 +9,14 @@ from .cdm import ConjunctionDataMessage
 
 def generate_dataset(dataset_dir, num_events, *args, **kwargs):
     model = ConstellationPair(*args, **kwargs)
-    def generate_event():
+    def generate_conjunction():
         found = False
         while not found:
             trace = model.get_trace()
             if trace['conj']:
                 found = True
-        cdms = trace['cdms']
-        print('Generated event with {} CDMs'.format(len(cdms)))
-        return trace['cdms']
+        print('Generated event with {} CDMs'.format(len(trace['cdms'])))
+        return trace
 
     print('Generating CDM dataset')
     print('Directory: {}'.format(dataset_dir))
@@ -24,9 +24,15 @@ def generate_dataset(dataset_dir, num_events, *args, **kwargs):
     for i in range(num_events):
         print('Generating event {} / {}'.format(i+1, num_events))
         file_name_event = os.path.join(dataset_dir, 'event_{}'.format(str(uuid.uuid4())))
-        cdms = generate_event()
+        
+        trace = generate_conjunction()
+        file_name_trace = file_name_event + '.trace'
+        print('Saving trace: {}'.format(file_name_trace))
+        torch.save(trace, file_name_trace)
+
+        cdms = trace['cdms']
         for j, cdm in enumerate(cdms):
             file_name_suffix = '{}'.format(j).rjust(len('{}'.format(len(cdms))), '0')
             file_name_cdm = file_name_event + '_{}.cdm.kvn.txt'.format(file_name_suffix)
-            print('Saving cdm: {}'.format(file_name_cdm))
+            print('Saving cdm  : {}'.format(file_name_cdm))
             cdm.save(file_name_cdm)
